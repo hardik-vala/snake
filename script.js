49c841d8762175637c1c50a76fa7325b92a04dcf
Hardik Vala <hardik.vala@mail.mcgill.ca>
2016-09-15 23:29:29 -0400

    Expanded canvas to fullscreen

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 	c = document.getElementById("mainCanvas");
 	var ctx = c.getContext("2d");
+	ctx.canvas.width = window.innerWidth;
+	ctx.canvas.height = window.innerHeight;
+
 	ctx.beginPath();
 	ctx.arc(95, 50, 40, 0, 2*Math.PI);
 	ctx.stroke();

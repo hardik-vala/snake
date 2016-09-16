c3a0df4c0c96d373b69c2e6de5eecddb0cc401c9
Hardik Vala <hardik.vala@mail.mcgill.ca>
2016-09-15 23:23:13 -0400

    Fixed head of index.html and moved javascript to separate file

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	c = document.getElementById("mainCanvas");
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(95, 50, 40, 0, 2*Math.PI);
+	ctx.stroke();
+});

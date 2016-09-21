5c33460be32058b1eb3842d0ef4917385f71ef3e
Hardik Vala <hardik.vala@mail.mcgill.ca>
2016-09-20 23:47:58 -0400

    Commented JS

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 	var c = $("#mainCanvas")[0];
 	var ctx = c.getContext("2d");
+	// Stretch canvas to full screen.
 	ctx.canvas.width = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
 
+	// Draw random circle.
 	ctx.beginPath();
 	ctx.arc(95, 50, 40, 0, 2*Math.PI);
 	ctx.stroke();
